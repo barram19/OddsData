1a81a0898b85012765e639437739f34d08fa8786
barram19 <131893869+barram19@users.noreply.github.com>
2024-03-15 15:55:38 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,26 +2,16 @@ document.addEventListener('DOMContentLoaded', function() {
     // Display a loading message or spinner
     document.getElementById('apiResponse').innerHTML = '<p>Loading odds...</p>';
 
-   fetch('https://us-central1-cbbbot-413503.cloudfunctions.net/post_sports_odds_web', {
-        method: 'OPTIONS',
-        headers: {
-            'Content-Type': 'html',
-        },
-    })
-    .then(response => {
-        if (response.ok) {
-            return fetch('https://us-central1-cbbbot-413503.cloudfunctions.net/post_sports_odds_web', {
-        method: 'GET' // Changed from 'OPTIONS' to 'GET'
-        // Removed headers since you're expecting HTML response, not JSON
-    })
+    fetch('https://us-central1-cbbbot-413503.cloudfunctions.net/post_sports_odds_web') // Default to GET method
     .then(response => {
         if (!response.ok) {
+            // If the server response was not OK, throw an error to be caught in the catch block
             throw new Error(`Network response was not ok: ${response.statusText}`);
         }
+        return response.text(); // Process the response body as text (HTML content)
     })
-
     .then(html => {
-        // Assuming the server responds with HTML, display it
+        // Display the HTML content returned by the server
         document.getElementById('apiResponse').innerHTML = html;
     })
     .catch(error => {
