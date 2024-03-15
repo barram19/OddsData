9789a667be9caf390c2b313c1738912483c8c010
barram19 <131893869+barram19@users.noreply.github.com>
2024-03-15 15:13:00 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 document.addEventListener('DOMContentLoaded', function() {
+    // Display a loading message or spinner
+    document.getElementById('apiResponse').innerHTML = '<p>Loading odds...</p>';
+    
     fetch('https://us-central1-cbbbot-413503.cloudfunctions.net/post_sports_odds_web')
-    .then(response => response.text()) // Assuming the server responds with HTML
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.text();
+    })
     .then(html => {
+        // Assuming the server responds with HTML, display it
         document.getElementById('apiResponse').innerHTML = html;
     })
-    .catch(error => console.error('Error:', error));
+    .catch(error => {
+        console.error('Error:', error);
+        // Display a user-friendly error message on the webpage
+        document.getElementById('apiResponse').innerHTML = '<p>Failed to load odds. Please try again later.</p>';
+    });
 });
