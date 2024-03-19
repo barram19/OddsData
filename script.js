3a16c69888b6f4bee522e569db9af1a4148b6b88
barram19 <131893869+barram19@users.noreply.github.com>
2024-03-18 21:20:44 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
     fetch('https://us-central1-cbbbot-413503.cloudfunctions.net/post_sports_odds_webpage', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            content: "Fetch upcoming games and odds"
-        })
-    })
-    .then(response => response.text()) // Change this to .text()
-    .then(html => {
-        document.getElementById('oddsDisplay').innerHTML = html; // Directly use the HTML response
-    })
-    .catch((error) => {
-        console.error('Error:', error);
-        document.getElementById('oddsDisplay').innerHTML = '<p>Error fetching odds.</p>';
-    });
-});
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+    }
+})
+.then(response => response.text())
+...
